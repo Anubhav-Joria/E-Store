5a4506beb0c1948b626b729bc087d46671dce0cb
Anubhav-Joria <91547784+Anubhav-Joria@users.noreply.github.com>
2022-10-13 23:01:02 +0530

    Updates

---
+++ precache-manifest.077068e8c3bd6748f5cdeb2047d586cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a6d037170f555edcf2ad8239e83c89",
+    "url": "/E-Store/index.html"
+  },
+  {
+    "revision": "762ff8499f620135ebd2",
+    "url": "/E-Store/static/js/2.4917870e.chunk.js"
+  },
+  {
+    "revision": "a712f5023b98f1ee6f879156125341ec",
+    "url": "/E-Store/static/js/2.4917870e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bbda9259ec64f10ef4d",
+    "url": "/E-Store/static/js/main.c1dc20b0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/E-Store/static/js/main.c1dc20b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5291c4a91fdfad1b68fc",
+    "url": "/E-Store/static/js/runtime-main.1e2b9942.js"
+  }
+]);
